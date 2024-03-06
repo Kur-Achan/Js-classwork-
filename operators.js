9ca158a1d0f0cb36cf3c7f8272ddb0ab700c477e
Kur-Achan <you@example.com>
2024-03-06 19:50:36 +0300

    add js files

---
+++ operators.js
@@ -0,0 +1,58 @@
+let a = 20;
+let b =30;
+let add = a + b
+console.log({add});
+
+
+let subtract = a - b;
+console.log({subtract});
+
+let division = a / b;
+console.log({division});
+
+
+let multiply = a* b;
+console.log({multiply});
+
+let c = '20';
+let looselyEqualTo =  a == c;
+console.log({looselyEqualTo});
+
+let strictlyEqalTo = a=== c;
+console.log({strictlyEqalTo});
+
+let notEqualTo = a != c;
+console.log({notEqualTo}); 
+
+let strictlyNotEqualTo = a !== c;
+console.log({strictlyNotEqualTo});
+
+// Increment
+a++;
+console.log({a});
+
+// Decrement
+
+b--;
+console.log({b});
+
+a+=5;
+console.log({a});
+
+//  Implicit coertion
+let d = 1 * c;
+console.log({d});
+
+let e =+c;
+console.log({e});
+
+
+//Explicit coertion
+
+let f = Number(c);
+console.log({f});
+
+
+
+
+
